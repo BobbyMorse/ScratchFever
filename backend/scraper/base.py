20520982fcd0532bde3145594f648bfdf66ccd33
BobbyMorse <robertmorse73@gmail.com>
2026-05-10 19:36:47 -0400

    auto: 2026-05-10 19:36:47

--- backend/scraper/base.py
+++ backend/scraper/base.py
@@ -18,6 +18,8 @@ HEADERS = {
     ),
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.9",
+    "Cache-Control": "no-cache",
+    "Pragma": "no-cache",
 }
 
 
