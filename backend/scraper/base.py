dbee91f3494d3a55e5b3a26af06d93d379ccb375
BobbyMorse <robertmorse73@gmail.com>
2026-05-13 21:12:32 -0400

    auto: 2026-05-13 21:12:32

--- backend/scraper/base.py
+++ backend/scraper/base.py
@@ -49,7 +49,6 @@ class BaseScraper(ABC):
         ev_data = calculate_ev(price, tiers, tickets_remaining)
         top_prize, top_prize_remaining = find_top_prize(tiers)
         jackpot_odds = calculate_jackpot_odds(tiers, tickets_remaining)
-        top_prize_odds = calculate_top_prize_odds(tiers, tickets_remaining)
         return {
             "game_id": str(game_id),
             "name": name,
@@ -60,7 +59,6 @@ class BaseScraper(ABC):
             "top_prize": top_prize,
             "top_prize_remaining": top_prize_remaining,
             "jackpot_odds_one_in": jackpot_odds,
-            "top_prize_odds_one_in": top_prize_odds,
             "total_tickets": total_tickets,
             "tickets_remaining": tickets_remaining,
             "detail_url": detail_url,
