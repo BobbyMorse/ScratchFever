db96c1a79a59e7f7e0f1a386715402c8d395b244
BobbyMorse <robertmorse73@gmail.com>
2026-05-13 20:32:29 -0400

    auto: 2026-05-13 20:32:29

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -635,7 +635,7 @@ function renderTable() {
     games = games.filter(g => {
       if (!g.total_tickets || !g.tickets_remaining) return true;
       const pctLeft = g.tickets_remaining / g.total_tickets;
-      return !(pctLeft < 0.05 && g.return_pct >= 100);
+      return !(pctLeft < 0.02 && g.return_pct >= 100);
     });
   }
 
