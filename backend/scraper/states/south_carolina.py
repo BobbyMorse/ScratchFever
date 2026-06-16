ff549406a306deb736573d1c00d6573a1cf22572
BobbyMorse <robertmorse73@gmail.com>
2026-06-15 20:29:26 -0400

    auto: 2026-06-15 20:29:26

--- backend/scraper/states/south_carolina.py
+++ backend/scraper/states/south_carolina.py
@@ -14,7 +14,9 @@ EV computed from remaining prize data.
 """
 import re
 import logging
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import as_completed
+
+from backend.scraper._shared_pool import DETAIL_POOL
 from backend.scraper.base import BaseScraper
 from backend.ev_calculator import parse_prize_amount, parse_odds
 
